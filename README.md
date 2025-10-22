716d7222d9bf40e39701c862ad042de90e478be1
lisiur <lisiurday@gmail.com>
2025-10-22 11:45:23 +0800

    feat: update

---
+++ README.md
@@ -0,0 +1,30 @@
+cargo features --help
+
+```bash
+Manage workspace dependencies
+
+Usage: cargo features [OPTIONS] [COMMAND]
+
+Commands:
+  list  List all dependencies
+  help  Print this message or the help of the given subcommand(s)
+
+Options:
+  -p, --package <PACKAGE>        Workspace package name
+  -d, --dependency <DEPENDENCY>  Dependency name
+  -h, --help                     Print help
+```
+
+cargo features list --help
+
+```bash
+List workspace dependencies
+
+Usage: cargo features list [OPTIONS]
+
+Options:
+  -p, --package <PACKAGE>        Workspace package name
+  -d, --dependency <DEPENDENCY>  Dependency name
+  -a, --all
+  -h, --help                     Print help
+```
--- src/bin/features.rs
+++ src/bin/features.rs
@@ -1,10 +1,9 @@
 use std::{cmp::Ordering, fmt::Display, fs, process};
 
-use color_eyre::Result;
-use colored::Colorize;
-
+use anyhow::Result;
 use cargo_metadata::{Dependency, Metadata, MetadataCommand, Package};
 use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
 use nucleo_matcher::{Config, Matcher, pattern::Atom};
 use toml_edit::DocumentMut;
 
@@ -20,22 +19,65 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Features(FeatuerArgs),
+    /// Manage workspace dependencies
+    Features(FeatureArgs),
 }
 
 #[derive(Args)]
-struct FeatuerArgs {
+struct FeatureArgs {
     /// Workspace package name
-    #[arg(short, long, value_name = "PACKAGE_NAME")]
+    #[arg(short, long, value_name = "PACKAGE")]
     package: Option<String>,
 
     /// Dependency name
+    #[arg(short, long, value_name = "DEPENDENCY")]
     dependency: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<FeatureCommands>,
+}
+
+#[derive(Subcommand)]
+enum FeatureCommands {
+    /// List workspace dependencies
+    List(FeatureListArgs),
+}
+
+#[derive(Args)]
+struct FeatureListArgs {
+    /// Workspace package name
+    #[arg(short, long, value_name = "PACKAGE")]
+    package: Option<String>,
+
+    /// Dependency name
+    #[arg(short, long, value_name = "DEPENDENCY")]
+    dependency: Option<String>,
+
+    #[arg(short, long, default_value_t = false)]
+    all: bool,
 }
 
 struct Feature {
     name: String,
     includes: Vec<String>,
+}
+
+struct DependencyFeatures {
+    features: Vec<Feature>,
+    enabled_features: Vec<String>,
+}
+
+impl DependencyFeatures {
+    fn uses_default_features(&self) -> bool {
+        self.enabled_features.contains(&"default".to_string())
+    }
+
+    fn enabled_features_except_default(&self) -> Vec<&String> {
+        self.enabled_features
+            .iter()
+            .filter(|x| x.ne(&"default"))
+            .collect::<Vec<_>>()
+    }
 }
 
 impl Display for Feature {
@@ -46,6 +88,42 @@ impl Display for Feature {
             self.name.blue(),
             format!(" = [{}]", self.includes.join(", ")).bright_black()
         )
+    }
+}
+
+fn get_dependency_features(metadata: &Metadata, dependency: &Dependency) -> DependencyFeatures {
+    let mut enabled_features = dependency.features.clone();
+
+    let dep_package = metadata
+        .packages
+        .iter()
+        .find(|x| x.name == dependency.name)
+        .unwrap();
+
+    if dep_package.features.get("default").is_some() && dependency.uses_default_features {
+        enabled_features.insert(0, "default".to_string());
+    }
+
+    let mut all_features = dep_package
+        .features
+        .iter()
+        .map(|x| Feature {
+            name: x.0.to_string(),
+            includes: x.1.clone(),
+        })
+        .collect::<Vec<_>>();
+
+    all_features.sort_by(|a, b| {
+        if a.name.eq("default") {
+            Ordering::Less
+        } else {
+            a.name.cmp(&b.name)
+        }
+    });
+
+    DependencyFeatures {
+        features: all_features,
+        enabled_features,
     }
 }
 
@@ -142,113 +220,161 @@ fn choose_dependency(package: &Package, keyword: Option<String>) -> Result<&Depe
     Ok(dependency)
 }
 
-fn choose_features(metadata: &Metadata, dependency: &Dependency) -> Result<(bool, Vec<String>)> {
-    let mut enabled_features = dependency.features.clone();
-    let uses_default_features = dependency.uses_default_features;
+fn choose_features(metadata: &Metadata, dependency: &Dependency) -> Result<DependencyFeatures> {
+    let mut dependency_features = get_dependency_features(metadata, dependency);
 
-    let package = metadata
-        .packages
-        .iter()
-        .find(|x| x.name == dependency.name)
-        .unwrap();
-    let package_features = &package.features;
-    let package_default_features = package_features
-        .get("default")
-        .map(|x| x.iter().cloned().collect::<Vec<String>>());
-
-    if package_default_features.is_some() && uses_default_features {
-        enabled_features.insert(0, "default".to_string());
-    }
-
-    let mut toggle_features_options = package_features
-        .iter()
-        .map(|x| Feature {
-            name: x.0.to_string(),
-            includes: x.1.clone(),
-        })
-        .collect::<Vec<_>>();
-
-    toggle_features_options.sort_by(|a, b| {
-        if a.name.eq("default") {
-            Ordering::Less
-        } else {
-            a.name.cmp(&b.name)
-        }
-    });
+    let toggle_features_options = dependency_features.features.iter().collect::<Vec<_>>();
 
     let default_index = toggle_features_options
         .iter()
         .enumerate()
-        .filter(|(_i, x)| enabled_features.contains(&x.name))
+        .filter(|(_i, x)| dependency_features.enabled_features.contains(&x.name))
         .map(|(i, _x)| i)
         .collect::<Vec<usize>>();
 
     let result = inquire::MultiSelect::new("Toggle features", toggle_features_options)
         .with_default(&default_index)
         .prompt();
+
     let ans = handle_prompt_result(result)?;
 
-    let enabled_features = ans.into_iter().map(|x| x.name).collect::<Vec<String>>();
-    let uses_default_features =
-        package_default_features.is_none() || enabled_features.iter().any(|x| x == "default");
-
-    let enabled_features_except_default = enabled_features
+    let enabled_features = ans
         .into_iter()
-        .filter(|x| x.ne(&"default"))
-        .collect::<Vec<_>>();
+        .map(|x| x.name.clone())
+        .collect::<Vec<String>>();
 
-    Ok((uses_default_features, enabled_features_except_default))
+    dependency_features.enabled_features = enabled_features;
+
+    Ok(dependency_features)
 }
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+fn manage_features(package: Option<String>, dependency: Option<String>) -> Result<()> {
+    let metadata = MetadataCommand::new().exec()?;
 
-    let cli = Cli::parse();
+    let workspace_package = choose_workspace_package(&metadata, package)?;
 
-    if let Some(Commands::Features(args)) = cli.command {
-        let metadata = MetadataCommand::new().exec().unwrap();
+    let dependency = choose_dependency(workspace_package, dependency)?;
+    let dependency_name = dependency.name.as_str();
 
-        let workspace_package = choose_workspace_package(&metadata, args.package)?;
+    let dependency_features = choose_features(&metadata, dependency)?;
+    let uses_default_features = dependency_features.uses_default_features();
 
-        let dependency = choose_dependency(workspace_package, args.dependency)?;
-        let dependency_name = dependency.name.as_str();
+    let toml_path = &workspace_package.manifest_path;
+    let toml_plaintext = fs::read_to_string(toml_path)?;
+    let mut doc = toml_plaintext.parse::<DocumentMut>()?;
 
-        let (uses_default_features, enabled_features) = choose_features(&metadata, dependency)?;
-
-        let toml_path = &workspace_package.manifest_path;
-        let toml_plaintext = fs::read_to_string(toml_path)?;
-        let mut doc = toml_plaintext.parse::<DocumentMut>()?;
-
-        let mut array = toml_edit::Array::default();
-        enabled_features.iter().for_each(|f| {
+    let mut array = toml_edit::Array::default();
+    dependency_features
+        .enabled_features_except_default()
+        .into_iter()
+        .for_each(|f| {
             array.push(f);
         });
 
-        let version = dependency
-            .req
-            .to_string()
-            .trim_start_matches('^')
-            .to_owned();
+    let version = dependency
+        .req
+        .to_string()
+        .trim_start_matches('^')
+        .to_owned();
 
-        if uses_default_features && array.is_empty() {
-            // only declar version
-            doc["dependencies"][dependency_name] = toml_edit::value(version);
-        } else {
-            // use inline table
-            doc["dependencies"][dependency_name] = toml_edit::value(toml_edit::InlineTable::new());
-            // set version
-            doc["dependencies"][dependency_name]["version"] = toml_edit::value(version);
-            if !uses_default_features {
-                // set default-features
-                doc["dependencies"][dependency_name]["default-features"] = toml_edit::value(false);
+    if uses_default_features && array.is_empty() {
+        // only declar version
+        doc["dependencies"][dependency_name] = toml_edit::value(version);
+    } else {
+        // use inline table
+        doc["dependencies"][dependency_name] = toml_edit::value(toml_edit::InlineTable::new());
+        // set version
+        doc["dependencies"][dependency_name]["version"] = toml_edit::value(version);
+        if !uses_default_features {
+            // set default-features
+            doc["dependencies"][dependency_name]["default-features"] = toml_edit::value(false);
+        }
+        if !array.is_empty() {
+            // set features
+            doc["dependencies"][dependency_name]["features"] = toml_edit::value(array);
+        }
+    }
+
+    fs::write(toml_path, doc.to_string())?;
+
+    Ok(())
+}
+
+fn list_workspace_features(args: FeatureListArgs) -> Result<()> {
+    let metadata = MetadataCommand::new().exec()?;
+    let workspace_packages = metadata.workspace_packages();
+    for package in workspace_packages {
+        if args
+            .package
+            .as_ref()
+            .map_or(false, |x| !x.contains(&package.name.to_string()))
+        {
+            continue;
+        }
+        println!("{:>0}{}:", "", package.name.cyan());
+        let dependencies = &package.dependencies;
+        for dependency in dependencies {
+            if args
+                .dependency
+                .as_ref()
+                .map_or(false, |x| !x.contains(&dependency.name))
+            {
+                continue;
             }
-            if !array.is_empty() {
-                // set features
-                doc["dependencies"][dependency_name]["features"] = toml_edit::value(array);
+
+            println!("{:>2}{}:", "", dependency.name);
+            let dependency_features = get_dependency_features(&metadata, dependency);
+            if args.all {
+                for feature in dependency_features.features {
+                    let enabled = dependency_features.enabled_features.contains(&feature.name);
+                    println!(
+                        "{:>4}{} {}",
+                        "",
+                        format!(
+                            "{}{}{}",
+                            "[".bright_black(),
+                            if enabled {
+                                "x".green().to_string()
+                            } else {
+                                " ".to_string()
+                            },
+                            "]".bright_black()
+                        ),
+                        feature
+                    );
+                }
+            } else {
+                for feature in dependency_features.enabled_features {
+                    println!(
+                        "{:>4}{}",
+                        "",
+                        Feature {
+                            name: feature,
+                            includes: dependency.features.clone(),
+                        }
+                    );
+                }
             }
         }
+    }
 
-        fs::write(toml_path, doc.to_string())?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    if let Some(Commands::Features(args)) = cli.command {
+        if let Some(feat_command) = args.command {
+            match feat_command {
+                FeatureCommands::List(list_args) => {
+                    list_workspace_features(list_args)?;
+                }
+            }
+            return Ok(());
+        } else {
+            manage_features(args.package, args.dependency)?;
+        }
     }
 
     Ok(())
